dd5e10540d77654016681062ea647e25169fc83f
brad <bcb1771@gmail.com>
2019-01-07 14:50:51 -0700

    1/7/19

--- Senior capstone project/Assets/LacerationScript.cs
+++ Senior capstone project/Assets/LacerationScript.cs
@@ -8,9 +8,10 @@ public class LacerationScript : MonoBehaviour {
     //works but needs to be timed better. you want to have continuous pressure on the wound for
      public GameObject cut;
 
-    int countdown = 100;
+    int countdown = 50;
 
-    void OnMouseOver(){
+    void OnMouseDown(){
+        Debug.Log("cut is compressed" +countdown);
         if (countdown > 0){
         countdown --;
         print(countdown);
