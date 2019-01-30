eba8945f2c267b402002cff7b6dceec231c170cf
Brad <bcb1771@gmail.com>
2019-01-29 21:39:48 -0700

    adding some old scripts for level design

---
+++ Senior capstone project/Assets/Pulse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pulse : MonoBehaviour {
+    
+    public GameObject Arties;
+    
+
+    void OnMouseOver()
+    {
+        Arties.SetActive(true);
+        print("beat");
+    }
+
+    void OnMouseExit()
+    {
+        Arties.SetActive(false);       
+    }   
+}
