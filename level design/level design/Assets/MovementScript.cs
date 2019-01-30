5b1a1b49edae1ec458a48450c90d75d7e024ab8b
brad <bcb1771@gmail.com>
2019-01-30 10:49:06 -0700

    adding scripts for level design

--- level design/level design/Assets/MovementScript.cs
+++ level design/level design/Assets/MovementScript.cs
@@ -4,40 +4,204 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
+	public int currentJumpNum = 0;
+	int maxJump = 1;
+	public float maxS = 5f;
 
-	public int moveSpeed = 1;
-
+	public float jumpHeight = 5f;
+	float moveVelocity;
+	public int GravityForces = -10;
+	
 	private Rigidbody rb;
 
 	public float waitTime = 0.1f;
-	
+
 	// Use this for initialization
-	void Start ()
+	void Start()
 	{
 		rb = GetComponent<Rigidbody>();
 		StartCoroutine(Mover());
 	}
 
-
-	/*private void FixedUpdate()
-	{*/
-		
-	IEnumerator Mover() {
+	IEnumerator Mover()
+	{
 		while (true)
 		{
+			Physics.gravity = new Vector3(0, GravityForces, 0);
+			//-----------------------------------------------------------------movement 
 
-			float HorzMove = Input.GetAxis("Horizontal");
-			float vertMove = Input.GetAxis("Vertical");
-			Vector3 movement = new Vector3(HorzMove, 0.0f, vertMove);
+			
+			float movex = Input.GetAxis("Horizontal");
+			float movez = Input.GetAxis("Vertical");
+			rb.velocity = new Vector3(movex * maxS, rb.velocity.y, movez *maxS);
 
-			rb.AddForce(movement * moveSpeed);
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				Jump();
+			}
 			yield return new WaitForSeconds(waitTime);
-
 		}
+	}
 
-		
+	void Jump()
+	{
+		if (currentJumpNum < maxJump)
+		{
+			rb.velocity = new Vector3(rb.velocity.x, jumpHeight, 0f);
+			currentJumpNum++;
+		}
+	}
+	
+	void OnCollisionEnter (Collision col)
+	{
+		currentJumpNum = 0;
+	}
+	
+}
+/*
+ * 	
+	private Animator myAnimator;
 
+	//-------------------lightthings
+	public Light rabbitLight;
+	public Color color0 = Color.white;
+	public Color color1 = Color.red;
+	public float colorLightTime = 0.5f;
+	//-----------------------------
+
+	public GameObject plusOne;
+	public GameObject ZeroIcon;
+	public GameObject GameOverObject;
+
+
+
+	void Start(){
+		myAnimator = GetComponent<Animator>();
+		gameObject.SetActive (true);
+		rabbitLight = GetComponent<Light>();
+		starCount = 0;
+	}
+
+	void FixedUpdate()
+	{
+		Physics.gravity = new Vector3 (0,GravityForces, 0);
+		//-----------------------------------------------------------------movement 
+
+			myAnimator.SetFloat ("maxS", Mathf.Abs (Input.GetAxis ("Horizontal")));
+			transform.Translate (new Vector3 (notMovingSpeed, 0, 0) * Time.deltaTime);
+			float move = Input.GetAxis ("Horizontal");
+			GetComponent<Rigidbody> ().velocity = new Vector2 (move * maxS, GetComponent<Rigidbody> ().velocity.y);
+			if (move > 0 && !right)
+				Flip ();
+			else if (move < 0 && right)
+				Flip ();
+	
+//--------------------------------------------------------------------------------------
+		//------------ boundry set up
+
+
+		//-------------
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			
+
+			Jump();
+		} 	
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+
+
+			Jump();
+		} 	
+
+		PrintingScore ();
+	
 	}
 
 
-}
+
+	//used to flip the sprite-----------------
+	void Flip()
+	{
+		right = !right;
+		Vector3 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+		notMovingSpeed *= -1;
+	}
+
+
+
+
+	void PrintingScore()
+	{
+		scoreText.text = "Stars " + starCount.ToString ();
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+
+		currentJumpNum = 0;
+
+		if (col.gameObject.tag == "Star") {
+			
+			starCount++;
+			//print (starCount);
+			Vector3 spawnPosition = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+			Quaternion spwanRotation = Quaternion.identity;
+			Instantiate (plusOne, spawnPosition, spwanRotation);
+
+
+		} 
+
+		if (col.gameObject.tag == "BoundryLeft") {
+			Destroy (gameObject);
+			//--------------------------------------------------------------
+			//enable the game over buttons here.
+			//-------------------------------------
+
+
+			GameOverObject.SetActive (true);
+		}
+
+		if (col.gameObject.tag == "Cactus") {
+
+			float t = Mathf.PingPong (Time.time, colorLightTime) / colorLightTime;
+
+			rabbitLight.color = Color.Lerp (color1, color0, t);
+
+			Vector3 spawnPosition = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+			Quaternion spwanRotation = Quaternion.identity;
+			Instantiate (ZeroIcon, spawnPosition, spwanRotation);
+
+
+		}
+
+	}
+
+	void OnCollisionExit (Collision col)
+	{
+		if(col.gameObject.tag == "Ground"){
+		}
+
+		if (col.gameObject.tag == "Cactus") {
+
+			
+			float t = Mathf.PingPong (Time.time, colorLightTime) / colorLightTime;
+			rabbitLight.color = Color.Lerp(color0, color1, t);
+
+
+
+			if (starCount < 1)
+			{
+				//starCount --;
+				//print (starCount);
+				starCount = 0;
+			}
+			else{
+				//starCount = 0;	
+				starCount --;
+			}
+
+		}
+
+	}
+	}*/
---
+++ level design/level design/Assets/TriggerFall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerFall : MonoBehaviour
+{
+	void OnCollisionEnter (Collision col)
+	{
+		//GetComponent<Rigidbody>().useGravity = true;
+		if (col.gameObject.tag == "Player")
+		{
+			gameObject.SetActive(false);
+			print("collided");
+		}
+		
+	}
+
+	
+}
