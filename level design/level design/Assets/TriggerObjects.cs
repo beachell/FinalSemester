f8004eafe212ae12ec83ad803a1060ddc904b832
Brad <bcb1771@gmail.com>
2019-01-31 22:00:09 -0700

    added trigger scripts

---
+++ level design/level design/Assets/TriggerObjects.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerObjects : MonoBehaviour {
+    public GameObject objectToDisappear;
+    
+    void Start()
+    {
+       
+    }
+
+    void OnMouseDown()
+    {
+        objectToDisappear.SetActive(false); 
+    }
+}
