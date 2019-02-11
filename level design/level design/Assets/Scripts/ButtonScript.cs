1073dc7796d3fea05d98872bd0d4437ed129887f
brad <bcb1771@gmail.com>
2019-02-11 11:09:31 -0700

    got the first person controller from the store and got the crane moving

--- level design/level design/Assets/Scripts/ButtonScript.cs
+++ level design/level design/Assets/Scripts/ButtonScript.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonScript : MonoBehaviour {
+public class ButtonScript : MonoBehaviour
+{
+
+	public GameObject Crane;
+
+	public float YMove = 0;
+	private float yMin = 0.7f;
+	private float yMax = 4.0f;
+	
+	public float ZMove = 0;
+	private float zMin = -4.5f;
+	private float zMax = 16.0f;
+
+	private void Start()
+	{
+		//GetComponent<Transform>(Crane).position;
+	}
 
 	// Use this for initialization
-	void Start () {
-		
+	private void OnMouseOver()
+	{
+		//if (gameObject.transform.localPosition )
+		Crane.transform.Translate(0,YMove,ZMove);
 	}
+
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
