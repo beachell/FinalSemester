b1161663866fd87f390630f221425f42a5c47a91
Brad <bcb1771@gmail.com>
2019-02-19 20:52:18 -0700

    weekend work.

---
+++ level design/level design/Assets/character.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character : MonoBehaviour
+{
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; //this locks the posistion of the cursor so it's not freaking out on computers like mine with multimonitors    
+    }
+
+    // replace with a coroutine
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float straffe = Input.GetAxis("Horizontal") * speed;
+       // translation *= Time.deltaTime;
+        transform.Translate(straffe, 0, translation);
+
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;//this unlocks the cursor to move around to multiple monitors
+    }
+}
