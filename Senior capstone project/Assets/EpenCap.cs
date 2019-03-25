ba98c3f338ce707f5e0607132aa97ef4b07a07d5
Brad <bcb1771@gmail.com>
2019-03-25 15:03:44 -0600

    second class

---
+++ Senior capstone project/Assets/EpenCap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EpenCap : MonoBehaviour
+{
+    float followDistance = 12;
+
+    public GameObject Epen;
+    public GameObject objectToActivate;
+   
+    public GameObject objectToDeactvate;
+    public GameObject objectToDeactvate2;
+
+
+
+
+
+    void OnMouseDrag(){
+        Vector3 mousePosition = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, followDistance);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = objPosition;
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (Epen.CompareTag("Epen"))
+        {
+            Epen.SetActive(false);
+            objectToActivate.SetActive(true);
+            objectToDeactvate2.SetActive(false);
+            objectToDeactvate.SetActive(false);
+        }
+    }
+}
