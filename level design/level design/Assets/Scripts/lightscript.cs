9131e6df05b41990c0e08a209016a52b77100794
Brad <bcb1771@gmail.com>
2019-04-07 22:19:17 -0600

    retextures added found a door triggers are broken.

--- level design/level design/Assets/Scripts/lightscript.cs
+++ level design/level design/Assets/Scripts/lightscript.cs
@@ -9,17 +9,20 @@ public class lightscript : MonoBehaviour {
     public float shortTime = 0.0f;
     public float longwait = 0.5f;
     public float intesLight = 1;
+    public float randIntes;
 void Start() {
     StartCoroutine(flickerLight());
+        
 }
  
 IEnumerator flickerLight() {
  
     while(flickering) {
-     float waittime = Random.Range(shortTime, longwait+1);
+        randIntes = Random.Range(1, intesLight + 1);
+        float waittime = Random.Range(shortTime, longwait+1);
 
      yield return new WaitForSeconds(waittime);
-        gameObject.GetComponent<Light>().intensity += intesLight;
+        gameObject.GetComponent<Light>().intensity += randIntes;
         yield return new WaitForSeconds(waittime);
         gameObject.GetComponent<Light>().intensity = 0;
     }
