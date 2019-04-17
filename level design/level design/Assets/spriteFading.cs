7a86a6a65430b7dc15b70ec2e886f7b89f8ac431
brad <bcb1771@gmail.com>
2019-04-17 11:31:58 -0600

    adding sprites setdressing

---
+++ level design/level design/Assets/spriteFading.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spriteFading : MonoBehaviour
+{
+    public bool onOff = true;
+    public GameObject objectToTurnOff;
+
+    private void Start()
+    {
+        StartCoroutine(light());
+    }
+
+    // Update is called once per frame
+    IEnumerator light()
+    {
+        while (true)
+        {
+            if (onOff == onOff)
+            {
+                yield return onOff = !onOff;
+                objectToTurnOff.SetActive(onOff);
+                print(onOff);
+            }
+            yield return new WaitForSeconds(1);
+            
+        }
+     
+    }
+}
